5139ce4d0628587d8e9833a8989b812ff09fdc45
Tyler Lampel <lampel13@gmail.com>
2022-05-04 12:27:23 -0400

    commit tree 87392c30988065c83b85653777ed5c114fe91398

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(cards, word) {
+    let messages = [];
+    for (let i = 0; i < cards.length; i++) {
+        messages.push(`Thank you, ${cards[i]}, for the wonderful ${word} gift!`)
+    }
+    return messages
+}
+writeCards(cards, word)
+
+function countDown(number) {
+   while (number > 0) {
+        console.log(number--)
+    }
+  console.log(number)
+}
